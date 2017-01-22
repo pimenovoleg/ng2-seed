507e1b4e9a785164285c9e103d0e5410c5379e49
Oleg Pimenov <pimenov.o@gmail.com>
2017-01-22 13:32:44 +0300

    up to ng4 beta 4 & rm old files

--- src/app/modules/pages/dashboard/dashboard.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-    selector: 'basic-tables',
-    encapsulation: ViewEncapsulation.None,
-    styles: [require('./dashboard.scss')],
-    template: require('./dashboard.tpl.html')
-})
-export class DashBoardComponent {}
